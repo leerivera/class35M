89dcdc6dca3a5cf6e45aed210090d52388a5a806
leerivera <leerivera80@gmail.com>
2022-05-20 23:01:48 -0400

    one

--- class35-materials/async-practice/js/main.js
+++ class35-materials/async-practice/js/main.js
@@ -142,3 +142,10 @@
 // }
 // getACuteDogPhoto()
 
+//Code 08
+// async function getACuteDogPhoto(){
+//     const res = await fetch('https://dog.ceo/api/breeds/image/random')
+//     const data = await res.json()
+//     console.log(data)
+// }
+// getACuteDogPhoto()
